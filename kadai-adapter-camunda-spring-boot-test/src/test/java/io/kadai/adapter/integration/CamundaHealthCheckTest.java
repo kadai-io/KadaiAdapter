fa49f2b90fb28532b32926d71a75cb85b6b9cc3a
Julian Bruder <julian.bruder@outlook.com>
2025-05-13 09:54:52 +0200

    KAD-AD-120/121: adjust tests

--- kadai-adapter-camunda-spring-boot-test/src/test/java/io/kadai/adapter/integration/CamundaHealthCheckTest.java
+++ kadai-adapter-camunda-spring-boot-test/src/test/java/io/kadai/adapter/integration/CamundaHealthCheckTest.java
@@ -4,27 +4,19 @@ import static io.kadai.adapter.integration.HealthCheckEndpoints.CAMUNDA_ENGINE_E
 import static io.kadai.adapter.integration.HealthCheckEndpoints.HEALTH_ENDPOINT;
 import static io.kadai.adapter.integration.HealthCheckEndpoints.OUTBOX_EVENTS_COUNT_ENDPOINT;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
-import io.kadai.adapter.monitoring.CamundaHealthCheck;
-import io.kadai.adapter.monitoring.SchedulerHealthCheck;
 import io.kadai.adapter.test.KadaiAdapterTestApplication;
 import io.kadai.common.test.security.JaasExtension;
 import io.kadai.common.test.security.WithAccessId;
-import java.time.Duration;
-import java.time.Instant;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -33,8 +25,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.client.ExpectedCount;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
@@ -44,40 +34,21 @@ import org.springframework.web.client.RestTemplate;
     webEnvironment = WebEnvironment.DEFINED_PORT)
 @AutoConfigureWebTestClient
 @ExtendWith(JaasExtension.class)
-@ContextConfiguration
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CamundaHealthCheckTest extends AbsIntegrationTest {
 
-  @Autowired private SchedulerHealthCheck schedulerHealthIndicator;
-
-  private MockRestServiceServer mockServer;
-
-  @Autowired private RestTemplate restTemplate;
-
-  @Autowired private CamundaHealthCheck camundaHealthIndicator;
-
   @Autowired private TestRestTemplate testRestTemplate;
+  @Autowired private RestTemplate restTemplate;
+  private MockRestServiceServer mockServer;
 
   @BeforeEach
   @WithAccessId(user = "admin")
-  void setUp() throws Exception {
+  void setUp() {
     mockServer = MockRestServiceServer.bindTo(restTemplate).ignoreExpectOrder(true).build();
-
-    SchedulerHealthCheck spySchedulerHealthCheck = schedulerHealthIndicator;
-    Instant validRunTime = Instant.now().minus(Duration.ofMinutes(5));
-    when(spySchedulerHealthCheck.health())
-        .thenReturn(Health.up().withDetail("Last Run", validRunTime).build());
   }
 
   @AfterEach
   void tearDown() {
     mockServer.reset();
-  }
-
-  @AfterAll
-  void triggerSetUp() {
-    isInitialised = false;
   }
 
   @Test
@@ -92,7 +63,7 @@ class CamundaHealthCheckTest extends AbsIntegrationTest {
 
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     assertThat(response.getBody())
-        .contains("\"Camunda Health\":{\"status\":\"UP\"")
+        .contains("\"camunda\":{\"status\":\"UP\"")
         .contains("\"Camunda Engines\":[{\"name\":\"default\"}]");
   }
 
@@ -108,7 +79,7 @@ class CamundaHealthCheckTest extends AbsIntegrationTest {
 
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SERVICE_UNAVAILABLE);
     assertThat(response.getBody())
-        .contains("\"Camunda Health\":{\"status\":\"DOWN\"")
+        .contains("\"camunda\":{\"status\":\"DOWN\"")
         .contains("\"Camunda Engine Error\":\"No engines found\"");
   }
 
@@ -127,7 +98,7 @@ class CamundaHealthCheckTest extends AbsIntegrationTest {
 
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SERVICE_UNAVAILABLE);
     assertThat(response.getBody())
-        .contains("\"Camunda Health\":{\"status\":\"DOWN\"")
+        .contains("\"camunda\":{\"status\":\"DOWN\"")
         .contains("\"Camunda Engine Error\":");
   }
 
--- kadai-adapter-camunda-spring-boot-test/src/test/java/io/kadai/adapter/integration/SchedulerHealthCheckTest.java
+++ kadai-adapter-camunda-spring-boot-test/src/test/java/io/kadai/adapter/integration/SchedulerHealthCheckTest.java
@@ -1,114 +1,47 @@
 package io.kadai.adapter.integration;
 
-import static io.kadai.adapter.integration.HealthCheckEndpoints.HEALTH_ENDPOINT;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import io.kadai.adapter.impl.LastSchedulerRun;
 import io.kadai.adapter.monitoring.SchedulerHealthCheck;
-import io.kadai.adapter.test.KadaiAdapterTestApplication;
-import io.kadai.common.test.security.JaasExtension;
 import java.time.Duration;
 import java.time.Instant;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest(
-    classes = KadaiAdapterTestApplication.class,
-    webEnvironment = WebEnvironment.DEFINED_PORT)
-@AutoConfigureWebTestClient
-@ExtendWith(JaasExtension.class)
-@ContextConfiguration
 class SchedulerHealthCheckTest extends AbsIntegrationTest {
 
-  @TestConfiguration
-  static class TestConfig {
-    @Bean
-    SchedulerHealthCheck schedulerHealthCheck() throws Exception {
-      LastSchedulerRun lastSchedulerRunMock = Mockito.mock(LastSchedulerRun.class);
-      Instant dummyRunTime = Instant.now().minus(Duration.ofMinutes(10));
-      when(lastSchedulerRunMock.getLastRunTime()).thenReturn(dummyRunTime);
-
-      SchedulerHealthCheck realHealthCheck = new SchedulerHealthCheck(lastSchedulerRunMock);
-      SchedulerHealthCheck spyHealthCheck = Mockito.spy(realHealthCheck);
-
-      return spyHealthCheck;
-    }
-  }
-
-  @Autowired private SchedulerHealthCheck schedulerHealthIndicator;
-
-  @Autowired private TestRestTemplate testRestTemplate;
-
-  private SchedulerHealthCheck spyHealthCheck;
+  private SchedulerHealthCheck schedulerHealthCheckSpy;
+  private LastSchedulerRun lastSchedulerRunSpy;
 
   @BeforeEach
   void setUp() {
-    spyHealthCheck = schedulerHealthIndicator;
+    this.lastSchedulerRunSpy = Mockito.spy(new LastSchedulerRun());
+    this.schedulerHealthCheckSpy = Mockito.spy(new SchedulerHealthCheck(lastSchedulerRunSpy));
   }
 
   @Test
-  void should_ReturnUp_When_SchedulerRuns() throws Exception {
-    LastSchedulerRun spy = Mockito.spy(new LastSchedulerRun());
+  void should_ReturnUp_When_SchedulerRuns() {
     Instant validRunTime = Instant.now().minus(Duration.ofMinutes(5));
-    when(spy.getLastRunTime()).thenReturn(validRunTime);
+    when(lastSchedulerRunSpy.getLastRunTime()).thenReturn(validRunTime);
 
-    SchedulerHealthCheck schedulerHealthCheck = new SchedulerHealthCheck(spy);
-    Health health = Health.up().withDetail("Last Run", spy.getLastRunTime()).build();
+    Health health =
+        Health.up().withDetail("Last Run", lastSchedulerRunSpy.getLastRunTime()).build();
 
-    assertThat(schedulerHealthCheck.health()).isEqualTo(health);
+    assertThat(schedulerHealthCheckSpy.health()).isEqualTo(health);
   }
 
   @Test
   void should_ReturnDown_When_SchedulerDoesNotRun() {
-    LastSchedulerRun spy = Mockito.spy(new LastSchedulerRun());
     Instant invalidRunTime = Instant.now().minus(Duration.ofMinutes(15));
-    when(spy.getLastRunTime()).thenReturn(invalidRunTime);
+    when(lastSchedulerRunSpy.getLastRunTime()).thenReturn(invalidRunTime);
 
-    SchedulerHealthCheck schedulerHealthCheck = new SchedulerHealthCheck(spy);
-    Health health = Health.down().withDetail("Last Run", spy.getLastRunTime()).build();
+    Health health =
+        Health.down().withDetail("Last Run", lastSchedulerRunSpy.getLastRunTime()).build();
 
-    assertThat(schedulerHealthCheck.health()).isEqualTo(health);
-  }
-
-  @Test
-  void should_ReturnUp_When_CallingHealthEndpoint() {
-    Instant validRunTime = Instant.now().minus(Duration.ofMinutes(5));
-    when(spyHealthCheck.health())
-        .thenReturn(Health.up().withDetail("Last Run", validRunTime).build());
-
-    ResponseEntity<String> response =
-        testRestTemplate.getForEntity(
-            HEALTH_ENDPOINT, String.class);
-
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(response.getBody()).contains("Last Run", validRunTime.toString());
-  }
-
-  @Test
-  void should_ReturnDown_When_CallingHealthEndpoint() {
-    Instant validRunTime = Instant.now().minus(Duration.ofMinutes(15));
-    when(spyHealthCheck.health())
-        .thenReturn(Health.down().withDetail("Last Run", validRunTime).build());
-
-    ResponseEntity<String> response =
-        testRestTemplate.getForEntity(
-            HEALTH_ENDPOINT, String.class);
-
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SERVICE_UNAVAILABLE);
-    assertThat(response.getBody()).contains("Last Run", validRunTime.toString());
+    assertThat(schedulerHealthCheckSpy.health()).isEqualTo(health);
   }
 }
