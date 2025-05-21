dd4b49d5276a6732d87369b77b8980b793906739
Julian Bruder <julian.bruder@outlook.com>
2025-05-21 20:26:50 +0200

    KAD-AD-99: address check-style

--- kadai-adapter/src/test/java/io/kadai/adapter/monitoring/SchedulerHealthIndicatorTest.java
+++ kadai-adapter/src/test/java/io/kadai/adapter/monitoring/SchedulerHealthIndicatorTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import io.kadai.adapter.impl.LastSchedulerRun;
-
 import java.time.Duration;
 import java.time.Instant;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,8 +28,7 @@ class SchedulerHealthIndicatorTest {
     Instant validRunTime = Instant.now().minus(Duration.ofMinutes(3));
     when(lastSchedulerRunSpy.getLastRunTime()).thenReturn(validRunTime);
 
-    Health health =
-        Health.up().withDetail("lastRun", lastSchedulerRunSpy.getLastRunTime()).build();
+    Health health = Health.up().withDetail("lastRun", lastSchedulerRunSpy.getLastRunTime()).build();
 
     assertThat(schedulerHealthIndicatorSpy.health()).isEqualTo(health);
   }
