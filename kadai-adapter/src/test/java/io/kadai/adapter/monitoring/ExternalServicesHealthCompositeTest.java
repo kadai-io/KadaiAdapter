de4dbb724bc2148f87b2efd415192241f38bbe94
Julian Bruder <julian.bruder@outlook.com>
2025-05-23 10:07:34 +0200

    KAD-AD-99: address arch-rule

--- kadai-adapter/src/test/java/io/kadai/adapter/monitoring/ExternalServicesHealthCompositeTest.java
+++ kadai-adapter/src/test/java/io/kadai/adapter/monitoring/ExternalServicesHealthCompositeTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class ExternalServicesHealthCompositeTest {
+class ExternalServicesHealthCompositeTest {
 
   @ParameterizedTest
   @MethodSource("externalServicesHealthConfigurationPropertiesProvider")
