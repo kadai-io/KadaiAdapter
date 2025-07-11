f570e82f5f6ba2bb79067868b43682b805e24076
Lea Haas <lea.haas@envite.de>
2025-07-11 17:46:26 +0200

    implementation of UserTaskCreation Listener

--- kadai-adapter-camunda-8-system-connector/src/main/java/io/kadai/adapter/systemconnector/camunda/tasklistener/UserTaskCreation.java
+++ kadai-adapter-camunda-8-system-connector/src/main/java/io/kadai/adapter/systemconnector/camunda/tasklistener/UserTaskCreation.java
@@ -3,8 +3,13 @@ package io.kadai.adapter.systemconnector.camunda.tasklistener;
 import io.camunda.client.api.response.ActivatedJob;
 import io.camunda.spring.client.annotation.JobWorker;
 import io.kadai.adapter.impl.KadaiTaskStarter;
+import io.kadai.adapter.systemconnector.api.ReferencedTask;
+import io.kadai.adapter.systemconnector.api.SystemConnector;
+import io.kadai.adapter.systemconnector.camunda.api.impl.Camunda8SystemConnectorImpl;
 import io.kadai.adapter.systemconnector.camunda.tasklistener.util.ReferencedTaskCreator;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -14,19 +19,22 @@ public class UserTaskCreation {
 
   private final KadaiTaskStarter taskStarter;
   private final ReferencedTaskCreator referencedTaskCreator;
+  private final Camunda8SystemConnectorImpl
+      systemConnector; // todo: it is not really nice to have this here, but it is needed
 
   private static final Logger LOGGER = LoggerFactory.getLogger(UserTaskCreation.class);
   private boolean gotActivated = false;
 
   public UserTaskCreation(
-      KadaiTaskStarter taskStarter, ReferencedTaskCreator referencedTaskCreator) {
+      KadaiTaskStarter taskStarter,
+      ReferencedTaskCreator referencedTaskCreator,
+      Camunda8SystemConnectorImpl systemConnector) {
     this.taskStarter = taskStarter;
     this.referencedTaskCreator = referencedTaskCreator;
+    this.systemConnector = systemConnector;
   }
 
   @JobWorker(type = "kadai-receive-task-created-event")
-  // todo: this is not working for some reason, it seems like Camunda is not reaching my job worker
-  // but no error is thrown
   public void receiveTaskCreatedEvent(final ActivatedJob job) {
 
     try {
@@ -38,9 +46,11 @@ public class UserTaskCreation {
                   + "connected to process instance {}",
               job.getProcessInstanceKey());
         }
-      }
 
-      // todo: handle task creation here
+        ReferencedTask referencedTask = referencedTaskCreator.createReferencedTaskFromJob(job);
+        taskStarter.createAndStartKadaiTasks(
+            systemConnector, new ArrayList<>(List.of(referencedTask)));
+      }
 
     } catch (Exception e) {
       LOGGER.error(
