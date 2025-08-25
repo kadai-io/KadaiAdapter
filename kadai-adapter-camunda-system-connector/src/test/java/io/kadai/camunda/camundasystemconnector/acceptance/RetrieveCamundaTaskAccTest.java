538b7ebef21c15541e144982df992a3befcbcb0e
James Reynaldi <james.reynaldi@envite.de>
2025-08-25 16:51:04 +0800

    Changed RetrieveCamundaTaskAccTest to be integration test

--- kadai-adapter-camunda-system-connector/src/test/java/io/kadai/camunda/camundasystemconnector/acceptance/RetrieveCamundaTaskAccTest.java
+++ kadai-adapter-camunda-system-connector/src/test/java/io/kadai/camunda/camundasystemconnector/acceptance/RetrieveCamundaTaskAccTest.java
@@ -19,62 +19,65 @@
 package io.kadai.camunda.camundasystemconnector.acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.contains;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.kadai.adapter.camunda.outbox.rest.CamundaTaskEvent;
-import io.kadai.adapter.camunda.outbox.rest.CamundaTaskEventListResource;
 import io.kadai.adapter.systemconnector.api.ReferencedTask;
 import io.kadai.adapter.systemconnector.camunda.api.impl.CamundaTaskRetriever;
-import io.kadai.adapter.systemconnector.camunda.api.impl.HttpHeaderProvider;
-import java.util.ArrayList;
+import io.kadai.camunda.camundasystemconnector.configuration.CamundaConnectorTestConfiguration;
 import java.util.List;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 
 /**
  * Unit test class for Camunda System Connector.
  *
  * @author bbr
  */
-@ExtendWith(MockitoExtension.class)
+@ContextConfiguration(classes = {CamundaConnectorTestConfiguration.class})
+@SpringBootTest
 class RetrieveCamundaTaskAccTest {
 
-  @Mock RestClient restClient;
+  @Autowired CamundaTaskRetriever taskRetriever;
 
-  @Mock HttpHeaderProvider httpHeaderProvider;
+  private MockWebServer mockWebServer;
 
-  @Mock ObjectMapper objectMapper;
+  @BeforeEach
+  void setUp() {
+    mockWebServer = new MockWebServer();
+    try {
+      mockWebServer.start();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 
-  @InjectMocks CamundaTaskRetriever taskRetriever;
+  @AfterEach
+  void tearDown() {
+    try {
+      mockWebServer.shutdown();
+    } catch (Exception e) {
+      // ignore
+    }
+  }
 
   @Test
-  void should_GetActiveCamundaTask() throws JsonProcessingException {
-
-    HttpHeaders mockHeaders = new HttpHeaders();
-    when(httpHeaderProvider.getHttpHeadersForOutboxRestApi()).thenReturn(mockHeaders);
-
-    String taskId = "801aca2e-1b25-11e9-b283-94819a5b525c";
+  void should_GetActiveCamundaTask() {
     String timeStamp = "2019-01-14T15:22:30.811+0000";
-
     ReferencedTask expectedTask = new ReferencedTask();
-    expectedTask.setId(taskId);
-    expectedTask.setCreated(timeStamp);
+    expectedTask.setOutboxEventId("1");
+    expectedTask.setOutboxEventType("create");
+    expectedTask.setId("801aca2e-1b25-11e9-b283-94819a5b525c");
     expectedTask.setName("modify Request");
     expectedTask.setAssignee("admin");
     expectedTask.setOwner("admin");
     expectedTask.setDescription("blabla");
+    expectedTask.setCreated(timeStamp);
     expectedTask.setPriority("50");
     expectedTask.setTaskDefinitionKey("Task_0yogl0i");
     expectedTask.setClassificationKey("Schaden_1");
@@ -87,104 +90,87 @@ class RetrieveCamundaTaskAccTest {
     expectedTask.setCustomInt6("6");
     expectedTask.setCustomInt7("7");
     expectedTask.setCustomInt8("8");
-    expectedTask.setOutboxEventId("1");
-    expectedTask.setOutboxEventType("create");
-
-    when(objectMapper.readValue(any(String.class), eq(ReferencedTask.class)))
-        .thenReturn(expectedTask);
-
-    RestClient.RequestHeadersUriSpec mockRequestSpec = mock(RestClient.RequestHeadersUriSpec.class);
-    RestClient.ResponseSpec mockResponseSpec = mock(RestClient.ResponseSpec.class);
-
-    when(restClient.get()).thenReturn(mockRequestSpec);
-    when(mockRequestSpec.uri(contains("/events?type=create"))).thenReturn(mockRequestSpec);
-    when(mockRequestSpec.headers(any())).thenReturn(mockRequestSpec);
-    when(mockRequestSpec.retrieve()).thenReturn(mockResponseSpec);
-    when(mockResponseSpec.toEntity(CamundaTaskEventListResource.class))
-        .thenReturn(ResponseEntity.ok(createMockCamundaTaskEventListResource("create", 1, taskId)));
-
-    List<ReferencedTask> actualResult =
-        taskRetriever.retrieveNewStartedCamundaTasks("http://localhost:8080", "default", null);
-
+    String expectedReplyBody =
+        "{"
+            + "\n"
+            + "\"camundaTaskEvents\":"
+            + "[\n "
+            + " {\n"
+            + "   \"id\": 1,\n"
+            + "   \"type\": \"create\",\n"
+            + "   \"systemEngineIdentifier\": \"default\",\n"
+            + "   \"created\": \"1970-01-01T10:48:16.436+0100\",\n"
+            + "   \"payload\": "
+            + " \"{\\\"id\\\":\\\"801aca2e-1b25-11e9-b283-94819a5b525c\\\","
+            + "            \\\"created\\\":\\\"2019-01-14T15:22:30.811+0000\\\","
+            + "            \\\"priority\\\":\\\"50\\\","
+            + "            \\\"name\\\":\\\"modify Request\\\","
+            + "            \\\"assignee\\\":\\\"admin\\\","
+            + "            \\\"description\\\":\\\"blabla\\\","
+            + "            \\\"owner\\\":\\\"admin\\\","
+            + "            \\\"taskDefinitionKey\\\":\\\"Task_0yogl0i\\\","
+            + "            \\\"classificationKey\\\":\\\"Schaden_1\\\","
+            + "            \\\"domain\\\":\\\"DOMAIN_B\\\","
+            + "            \\\"customInt1\\\":\\\"1\\\","
+            + "            \\\"customInt2\\\":\\\"2\\\","
+            + "            \\\"customInt3\\\":\\\"3\\\","
+            + "            \\\"customInt4\\\":\\\"4\\\","
+            + "            \\\"customInt5\\\":\\\"5\\\","
+            + "            \\\"customInt6\\\":\\\"6\\\","
+            + "            \\\"customInt7\\\":\\\"7\\\","
+            + "            \\\"customInt8\\\":\\\"8\\\""
+            + "           },\"\n"
+            + " }\n"
+            + "]"
+            + "}";
+    String camundaSystemUrl = mockWebServer.url("/").toString();
+    String systemEngineIdentifier = "default";
+    mockWebServer.enqueue(
+        new MockResponse()
+            .setHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            .setBody(expectedReplyBody));
+    List<ReferencedTask> actualResult = null;
+    try {
+      actualResult =
+          taskRetriever.retrieveNewStartedCamundaTasks(
+              camundaSystemUrl, systemEngineIdentifier, null);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
     assertThat(actualResult).isNotEmpty();
     assertThat(actualResult.get(0)).isEqualTo(expectedTask);
   }
 
   @Test
-  void should_GetFinishedCamundaTask() throws JsonProcessingException {
-
-    HttpHeaders mockHeaders = new HttpHeaders();
-    when(httpHeaderProvider.getHttpHeadersForOutboxRestApi()).thenReturn(mockHeaders);
-
-    String taskId = "2275fb87-1065-11ea-a7a0-02004c4f4f50";
-
+  void should_GetFinishedCamundaTask() {
     ReferencedTask expectedTask = new ReferencedTask();
-    expectedTask.setId(taskId);
+    expectedTask.setId("2275fb87-1065-11ea-a7a0-02004c4f4f50");
     expectedTask.setOutboxEventId("16");
     expectedTask.setOutboxEventType("complete");
-
-    when(objectMapper.readValue(any(String.class), eq(ReferencedTask.class)))
-        .thenReturn(expectedTask);
-
-    RestClient.RequestHeadersUriSpec mockRequestSpec = mock(RestClient.RequestHeadersUriSpec.class);
-    RestClient.ResponseSpec mockResponseSpec = mock(RestClient.ResponseSpec.class);
-
-    when(restClient.get()).thenReturn(mockRequestSpec);
-    when(mockRequestSpec.uri(contains("/events?type=complete"))).thenReturn(mockRequestSpec);
-    when(mockRequestSpec.headers(any())).thenReturn(mockRequestSpec);
-    when(mockRequestSpec.retrieve()).thenReturn(mockResponseSpec);
-    when(mockResponseSpec.toEntity(CamundaTaskEventListResource.class))
-        .thenReturn(
-            ResponseEntity.ok(createMockCamundaTaskEventListResource("complete", 16, taskId)));
-
+    String expectedReplyBody =
+        "{"
+            + "\n"
+            + "\"camundaTaskEvents\":"
+            + "[\n"
+            + "    {\n"
+            + "        \"id\": 16,\n"
+            + "        \"type\": \"complete\",\n"
+            + "        \"systemEngineIdentifier\": \"default\",\n"
+            + "        \"created\": \"2019-11-26T16:55:52.460+0100\",\n"
+            + "        \"payload\": \"{\\\"id\\\":\\\"2275fb87-1065-11ea-a7a0-02004c4f4f50\\\"}\"\n"
+            + "    }\n"
+            + "]"
+            + "}";
+    String camundaSystemUrl = mockWebServer.url("").toString();
+    String camundaSystemEngineIdentifier = "default";
+    mockWebServer.enqueue(
+        new MockResponse()
+            .setHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            .setBody(expectedReplyBody));
     List<ReferencedTask> actualResult =
-        taskRetriever.retrieveFinishedCamundaTasks("http://localhost:8080", "default", null);
-
+        taskRetriever.retrieveFinishedCamundaTasks(
+            camundaSystemUrl, camundaSystemEngineIdentifier, null);
     assertThat(actualResult).isNotEmpty();
     assertThat(actualResult.get(0)).isEqualTo(expectedTask);
-  }
-
-  private CamundaTaskEventListResource createMockCamundaTaskEventListResource(
-      String type, int id, String payloadId) {
-
-    CamundaTaskEvent event = new CamundaTaskEvent();
-    event.setId(id);
-    event.setType(type);
-    event.setSystemEngineIdentifier("default");
-    event.setCreated("1970-01-01T10:48:16.436+0100");
-
-    if ("create".equals(type)) {
-      event.setPayload(
-          "{\"id\":\""
-              + payloadId
-              + "\","
-              + "\"created\":\"2019-01-14T15:22:30.811+0000\","
-              + "\"priority\":\"50\","
-              + "\"name\":\"modify Request\","
-              + "\"assignee\":\"admin\","
-              + "\"description\":\"blabla\","
-              + "\"owner\":\"admin\","
-              + "\"taskDefinitionKey\":\"Task_0yogl0i\","
-              + "\"classificationKey\":\"Schaden_1\","
-              + "\"domain\":\"DOMAIN_B\","
-              + "\"customInt1\":\"1\","
-              + "\"customInt2\":\"2\","
-              + "\"customInt3\":\"3\","
-              + "\"customInt4\":\"4\","
-              + "\"customInt5\":\"5\","
-              + "\"customInt6\":\"6\","
-              + "\"customInt7\":\"7\","
-              + "\"customInt8\":\"8\"}");
-    } else {
-      event.setPayload("{\"id\":\"" + payloadId + "\"}");
-    }
-
-    CamundaTaskEventListResource resource = new CamundaTaskEventListResource();
-    List<CamundaTaskEvent> events = new ArrayList<>();
-
-    events.add(event);
-    resource.setCamundaTaskEvents(events);
-
-    return resource;
   }
 }
