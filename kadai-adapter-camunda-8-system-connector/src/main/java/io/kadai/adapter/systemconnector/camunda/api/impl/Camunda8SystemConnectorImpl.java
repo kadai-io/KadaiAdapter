4f767695ce7b5f71e092c6b833e65843d5c51851
Luca-MarieLimbach <luca-marie.limbach@envite.de>
2025-09-12 10:50:17 +0200

    implement OutboundSystemConnector operations for Camunda 8

--- kadai-adapter-camunda-8-system-connector/src/main/java/io/kadai/adapter/systemconnector/camunda/api/impl/Camunda8SystemConnectorImpl.java
+++ kadai-adapter-camunda-8-system-connector/src/main/java/io/kadai/adapter/systemconnector/camunda/api/impl/Camunda8SystemConnectorImpl.java
@@ -1,37 +1,53 @@
 package io.kadai.adapter.systemconnector.camunda.api.impl;
 
+import io.kadai.adapter.configuration.AdapterSpringContextProvider;
 import io.kadai.adapter.systemconnector.api.OutboundSystemConnector;
 import io.kadai.adapter.systemconnector.api.ReferencedTask;
 import io.kadai.adapter.systemconnector.api.SystemResponse;
 import io.kadai.adapter.systemconnector.camunda.config.Camunda8System;
 
+/** Sample Implementation of Camunda8SystemConnector. */
 public class Camunda8SystemConnectorImpl implements OutboundSystemConnector {
 
   private final Camunda8System camunda8System;
 
+  static final String URL_GET_CAMUNDA8_USER_TASKS = "/v1/user-tasks/";
+  static final String URL_CAMUNDA8_ASSIGNMENT = "/assignment";
+  static final String URL_CAMUNDA8_COMPLETION = "/completion";
+  static final String URL_CAMUNDA8_UNCLAIM = "/assignee";
+
+  static final String BODY_CAMUNDA8_ASSIGN = "{\"assignee\": \"%s\", "
+          + "\"allowOverride\": true, "
+          + "\"action\": \"assign\"}";
+  static final String BODY_CAMUNDA8_COMPLETE = "{\"variables\": {}, \"action\": \"complete\"}";
+  static final String BODY_EMPTY_REQUEST = "{}";
+
+  private final Camunda8TaskClaimer taskClaimer;
+
+  private final Camunda8TaskCompleter taskCompleter;
+
+  private final Camunda8TaskClaimCanceler  taskClaimCanceler;
+
   public Camunda8SystemConnectorImpl(Camunda8System camunda8System) {
     this.camunda8System = camunda8System;
+    taskClaimer = AdapterSpringContextProvider.getBean(Camunda8TaskClaimer.class);
+    taskCompleter = AdapterSpringContextProvider.getBean(Camunda8TaskCompleter.class);
+    taskClaimCanceler = AdapterSpringContextProvider.getBean(Camunda8TaskClaimCanceler.class);
   }
 
   @Override
-  public SystemResponse completeReferencedTask(ReferencedTask task) {
-    // todo (https://github.com/kadai-io/KadaiAdapter/issues/175): implement: call Camunda 8 API to
-    // complete the task
-    return null;
+  public SystemResponse completeReferencedTask(ReferencedTask camundaTask) {
+    return taskCompleter.completeCamunda8Task(camundaTask);
   }
 
   @Override
-  public SystemResponse claimReferencedTask(ReferencedTask task) {
-    // todo (https://github.com/kadai-io/KadaiAdapter/issues/175): implement: call Camunda 8 API to
-    // claim the task
-    return null;
+  public SystemResponse claimReferencedTask(ReferencedTask camundaTask) {
+    return taskClaimer.claimCamunda8Task(camundaTask);
   }
 
   @Override
   public SystemResponse cancelClaimReferencedTask(ReferencedTask task) {
-    // todo (https://github.com/kadai-io/KadaiAdapter/issues/175): implement: call Camunda 8 API to
-    // cancel the claim of the task
-    return null;
+    return taskClaimCanceler.cancelClaimOfCamunda8Task(task);
   }
 
   @Override
---
+++ kadai-adapter-camunda-8-system-connector/src/main/java/io/kadai/adapter/systemconnector/camunda/api/impl/Camunda8TaskClaimer.java
@@ -0,0 +1,87 @@
+package io.kadai.adapter.systemconnector.camunda.api.impl;
+
+import io.kadai.adapter.systemconnector.api.ReferencedTask;
+import io.kadai.adapter.systemconnector.api.SystemResponse;
+import io.kadai.adapter.systemconnector.camunda.config.CamundaSystemUrls;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+/** Claims tasks in Camunda 8 through the Tasklist API that have been claimed in KADAI. */
+@Component
+public class Camunda8TaskClaimer {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(Camunda8TaskClaimer.class);
+
+  private final Camunda8HttpHeaderProvider httpHeaderProvider;
+  private final RestTemplate restTemplate;
+
+  public Camunda8TaskClaimer(Camunda8HttpHeaderProvider httpHeaderProvider,
+                             RestTemplate restTemplate) {
+    this.httpHeaderProvider = httpHeaderProvider;
+    this.restTemplate = restTemplate;
+  }
+
+  @Value("${kadai.adapter.camunda8.claiming.enabled:true}")
+  private boolean claimingEnabled;
+
+  @Value("${kadai.adapter.camunda8.tasklist.url:http://localhost:8081}") //ToDo: SystemURL
+  private String tasklistBaseUrl;
+
+  private boolean claimConfigLogged = false;
+
+  public SystemResponse claimCamunda8Task(
+        CamundaSystemUrls.SystemUrlInfo camundaSystemUrlInfo, ReferencedTask referencedTask) {
+
+    if (!claimConfigLogged) {
+      LOGGER.info("Synchronizing claim of tasks in KADAI to Camunda 8 is set to {}",
+              claimingEnabled);
+      claimConfigLogged = true;
+    }
+
+    if (claimingEnabled) {
+
+      StringBuilder requestUrlBuilder = new StringBuilder();
+
+      requestUrlBuilder
+                .append(tasklistBaseUrl) //ToDo: SystemURL
+                .append(Camunda8SystemConnectorImpl.URL_GET_CAMUNDA8_USER_TASKS)
+                .append(referencedTask.getId())
+                .append(Camunda8SystemConnectorImpl.URL_CAMUNDA8_ASSIGNMENT);
+
+      String requestBody = String.format(
+                Camunda8SystemConnectorImpl.BODY_CAMUNDA8_ASSIGN,
+                referencedTask.getAssignee()
+      );
+
+      HttpHeaders headers = httpHeaderProvider.getHttpHeadersForCamunda8TasklistApi();
+      HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+
+      try {
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(
+                requestUrlBuilder.toString(),
+                requestEntity,
+                String.class);
+
+        LOGGER.debug(
+                    "claimed camunda 8 task {}. Status code = {}",
+                    referencedTask.getId(),
+                    responseEntity.getStatusCode());
+
+        return new SystemResponse(responseEntity.getStatusCode(), null);
+
+      } catch (HttpStatusCodeException e) {
+        LOGGER.warn("Caught Exception when trying to claim camunda 8 task", e);
+        throw e;
+      }
+    }
+    return new SystemResponse(HttpStatus.OK, null);
+  }
+}
