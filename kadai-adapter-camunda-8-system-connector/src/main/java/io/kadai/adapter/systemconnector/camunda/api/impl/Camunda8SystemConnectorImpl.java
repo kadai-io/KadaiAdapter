c72aa229831a798f75d6710f7402dc852a482c53
Luca-MarieLimbach <luca-marie.limbach@envite.de>
2025-09-19 10:49:26 +0200

    resolve checkstyle issues

--- kadai-adapter-camunda-8-system-connector/src/main/java/io/kadai/adapter/systemconnector/camunda/api/impl/Camunda8SystemConnectorImpl.java
+++ kadai-adapter-camunda-8-system-connector/src/main/java/io/kadai/adapter/systemconnector/camunda/api/impl/Camunda8SystemConnectorImpl.java
@@ -57,7 +57,7 @@ public class Camunda8SystemConnectorImpl implements OutboundSystemConnector {
 
   @Override
   public String toString() {
-     return "Camunda8SystemConnectorImpl [systemUrl=" + camunda8System.getSystemUrl() +
-                ", tasklistUrl=" + camunda8System.getTasklistUrl() + "]";
+    return "Camunda8SystemConnectorImpl [systemUrl=" + camunda8System.getSystemUrl()
+             + ", tasklistUrl=" + camunda8System.getTasklistUrl() + "]";
   }
 }
