fb384713836b1adaa20fca8f50bb3777b5fd3231
Julian Bruder <julian.bruder@outlook.com>
2025-09-29 16:24:10 +0200

    #118/#186: refine example-test

--- kadai-adapter-camunda-8-system-connector/src/test/java/io/kadai/adapter/systemconnector/camunda/tasklistener/UserTaskCreationTest.java
+++ kadai-adapter-camunda-8-system-connector/src/test/java/io/kadai/adapter/systemconnector/camunda/tasklistener/UserTaskCreationTest.java
@@ -1,5 +1,7 @@
 package io.kadai.adapter.systemconnector.camunda.tasklistener;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.camunda.client.CamundaClient;
 import io.camunda.client.api.response.ProcessInstanceEvent;
 import io.camunda.process.test.api.CamundaAssert;
@@ -7,7 +9,11 @@ import io.camunda.process.test.api.CamundaProcessTestContext;
 import io.camunda.process.test.api.CamundaSpringProcessTest;
 import io.kadai.adapter.test.KadaiAdapterSpringBootTest;
 import io.kadai.adapter.test.KadaiAdapterTestUtil;
+import io.kadai.common.api.KadaiEngine;
 import io.kadai.common.test.security.WithAccessId;
+import io.kadai.task.api.TaskState;
+import io.kadai.task.api.models.TaskSummary;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.TestPropertySource;
@@ -15,15 +21,16 @@ import org.springframework.test.context.TestPropertySource;
 @CamundaSpringProcessTest
 @KadaiAdapterSpringBootTest
 @TestPropertySource("classpath:camunda8-test-application.properties")
-class UserTaskCompletionTest {
+class UserTaskCreationTest {
 
   @Autowired private CamundaClient client;
   @Autowired private CamundaProcessTestContext processTestContext;
   @Autowired private KadaiAdapterTestUtil kadaiAdapterTestUtil;
+  @Autowired private KadaiEngine kadaiEngine;
 
   @Test
   @WithAccessId(user = "admin")
-  void should_CompleteProcessInstance() throws Exception {
+  void should_CreateKadaiTaskAndCompleteProcessInstance() throws Exception {
     kadaiAdapterTestUtil.createWorkbasket("GPK_KSC", "DOMAIN_A");
 
     // given: the processes are deployed
@@ -44,5 +51,9 @@ class UserTaskCompletionTest {
 
     // then
     CamundaAssert.assertThat(processInstance).isCompleted();
+    final List<TaskSummary> actual =
+        kadaiEngine.getTaskService().createTaskQuery().externalIdIn("2251799813685309").list();
+    assertThat(actual).hasSize(1);
+    assertThat(actual.get(0).getState()).isEqualTo(TaskState.COMPLETED);
   }
 }
