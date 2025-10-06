906ae90efdf170b2bb614071b389b484d602cc03
Julian Bruder <julian.bruder@outlook.com>
2025-10-06 11:22:18 +0200

    task/#234: fix check-style

--- kadai-adapter-camunda-system-connector/src/main/java/io/kadai/adapter/monitoring/Camunda7HealthContributorFactory.java
+++ kadai-adapter-camunda-system-connector/src/main/java/io/kadai/adapter/monitoring/Camunda7HealthContributorFactory.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.HealthContributor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
