4e153e88212efc6e6665faafba90390f6d0ab7e5
Luca-MarieLimbach <luca-marie.limbach@envite.de>
2025-10-07 14:38:26 +0200

    add UserTaskCompletionTest

--- kadai-adapter-camunda-8-system-connector/src/test/java/io/kadai/adapter/systemconnector/camunda/tasklistener/UserTaskCompletionTest.java
+++ kadai-adapter-camunda-8-system-connector/src/test/java/io/kadai/adapter/systemconnector/camunda/tasklistener/UserTaskCompletionTest.java
@@ -9,23 +9,27 @@ import io.kadai.adapter.test.KadaiAdapterTestUtil;
 import io.kadai.common.api.KadaiEngine;
 import io.kadai.common.test.security.WithAccessId;
 import io.kadai.task.api.TaskState;
+import io.kadai.task.api.models.Task;
 import io.kadai.task.api.models.TaskSummary;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @KadaiAdapterCamunda8SpringBootTest
-class UserTaskCreationTest {
-
-  @Autowired private CamundaClient client;
-  @Autowired private KadaiAdapterTestUtil kadaiAdapterTestUtil;
-  @Autowired private KadaiEngine kadaiEngine;
+class UserTaskCompletionTest {
+  @Autowired
+  private CamundaClient client;
+  @Autowired
+  private KadaiAdapterTestUtil kadaiAdapterTestUtil;
+  @Autowired
+  private KadaiEngine kadaiEngine;
 
   @Test
   @WithAccessId(user = "admin")
-  void should_CreateKadaiTask() throws Exception {
+  void should_CompleteKadaiTask_When_CamundaTaskIsCompleted() throws Exception {
     kadaiAdapterTestUtil.createWorkbasket("GPK_KSC", "DOMAIN_A");
     kadaiAdapterTestUtil.createClassification("L11010", "DOMAIN_A");
+
     client
         .newDeployResourceCommand()
         .addResourceFromClasspath("processes/sayHello.bpmn")
@@ -41,8 +45,18 @@ class UserTaskCreationTest {
             .join();
 
     CamundaAssert.assertThat(processInstance).isActive();
-    final List<TaskSummary> actual = kadaiEngine.getTaskService().createTaskQuery().list();
-    assertThat(actual).hasSize(1);
-    assertThat(actual.get(0).getState()).isEqualTo(TaskState.READY);
+
+    final List<TaskSummary> tasks = kadaiEngine.getTaskService().createTaskQuery().list();
+    assertThat(tasks).hasSize(1);
+    assertThat(tasks.get(0).getState()).isEqualTo(TaskState.READY);
+
+    final Task kadaiTask = kadaiEngine.getTaskService().getTask(tasks.get(0).getId());
+    final String externalId = kadaiTask.getExternalId();
+    final long userTaskKey = Long.parseLong(externalId.substring(externalId.lastIndexOf("-") + 1));
+
+    client.newCompleteUserTaskCommand(userTaskKey).send().join();
+
+    final Task completedTask = kadaiEngine.getTaskService().getTask(kadaiTask.getId());
+    assertThat(completedTask.getState()).isEqualTo(TaskState.COMPLETED);
   }
 }
