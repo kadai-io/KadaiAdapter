9821fd6d7f199c5efee9d0e93356d5ee50f283b7
Julian Bruder <julian.bruder@outlook.com>
2025-10-13 14:28:04 +0200

    task#185: add C8 Health-Stack

--- kadai-adapter-camunda-8-system-connector/src/main/java/io/kadai/adapter/systemconnector/camunda/tasklistener/UserTaskCompletion.java
+++ kadai-adapter-camunda-8-system-connector/src/main/java/io/kadai/adapter/systemconnector/camunda/tasklistener/UserTaskCompletion.java
@@ -4,20 +4,26 @@ import io.camunda.client.api.response.ActivatedJob;
 import io.camunda.spring.client.annotation.JobWorker;
 import io.kadai.adapter.exceptions.TaskTerminationFailedException;
 import io.kadai.adapter.impl.service.KadaiTaskCompletionService;
+import io.kadai.adapter.monitoring.MonitoredComponent;
+import io.kadai.adapter.monitoring.MonitoredRun;
 import io.kadai.adapter.systemconnector.api.ReferencedTask;
 import io.kadai.adapter.systemconnector.camunda.tasklistener.util.ReferencedTaskCreator;
+import io.kadai.adapter.util.LowerMedian;
+import java.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserTaskCompletion {
-
-  private final KadaiTaskCompletionService taskTerminator;
-  private final ReferencedTaskCreator referencedTaskCreator;
+public class UserTaskCompletion implements MonitoredComponent {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(UserTaskCompletion.class);
   private static final String TASK_STATE_COMPLETED = "COMPLETED";
+
+  private final KadaiTaskCompletionService taskTerminator;
+  private final ReferencedTaskCreator referencedTaskCreator;
+  private final MonitoredRun monitoredRun = new MonitoredRun();
+  private final LowerMedian<Duration> runDurationLowerMedian = new LowerMedian<>(100);
 
   public UserTaskCompletion(
       KadaiTaskCompletionService taskTerminator, ReferencedTaskCreator referencedTaskCreator) {
@@ -28,13 +34,31 @@ public class UserTaskCompletion {
   @JobWorker(type = "kadai-receive-task-completed-event")
   public void receiveTaskCompletedEvent(final ActivatedJob job)
       throws TaskTerminationFailedException {
+    monitoredRun.start();
     LOGGER.info(
         "UserTaskListener kadai-receive-task-completed-event has been called, "
             + "connected to process instance '{}'",
         job.getProcessInstanceKey());
+    try {
+      ReferencedTask referencedTask = referencedTaskCreator.createReferencedTaskFromJob(job);
+      referencedTask.setTaskState(TASK_STATE_COMPLETED);
+      taskTerminator.terminateKadaiTask(referencedTask);
+      monitoredRun.succeed();
+    } catch (Exception e) {
+      monitoredRun.fail();
+      throw e;
+    } finally {
+      runDurationLowerMedian.add(monitoredRun.getDuration());
+    }
+  }
 
-    ReferencedTask referencedTask = referencedTaskCreator.createReferencedTaskFromJob(job);
-    referencedTask.setTaskState(TASK_STATE_COMPLETED);
-    taskTerminator.terminateKadaiTask(referencedTask);
+  @Override
+  public MonitoredRun getLastRun() {
+    return monitoredRun;
+  }
+
+  @Override
+  public Duration getExpectedRunDuration() {
+    return runDurationLowerMedian.get().orElse(Duration.ZERO);
   }
 }
