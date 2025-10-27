0548250b58c59ea1cbdc131283dd325e034ff27c
Julian Bruder <julian.bruder@outlook.com>
2025-10-27 10:30:47 +0100

    remove merge-parasites

--- kadai-adapter/src/main/java/io/kadai/adapter/configuration/health/ConfigurationPropertiesConfig.java
+++
@@ -1,8 +0,0 @@
-package io.kadai.adapter.configuration.health;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties
-public class ConfigurationPropertiesConfig {}
