b9331e843940e793277b66b9bfe037727fbf18ae
Julian Bruder <julian.bruder@outlook.com>
2025-11-27 21:54:53 +0100

    task#308: keep adding all "new" c8 test-instances

--- kadai-adapter-camunda8-parent/kadai-adapter-camunda-8-system-connector/src/test/java/io/kadai/adapter/systemconnector/camunda/Camunda8TestSetupListener.java
+++ kadai-adapter-camunda8-parent/kadai-adapter-camunda-8-system-connector/src/test/java/io/kadai/adapter/systemconnector/camunda/Camunda8TestSetupListener.java
@@ -2,39 +2,40 @@ package io.kadai.adapter.systemconnector.camunda;
 
 import io.camunda.client.CamundaClient;
 import io.kadai.adapter.manager.AdapterManager;
-import io.kadai.adapter.systemconnector.api.OutboundSystemConnector;
+import io.kadai.adapter.systemconnector.camunda.api.impl.Camunda8SystemConnectorImpl;
+import io.kadai.adapter.systemconnector.camunda.api.impl.Camunda8TaskClaimCanceler;
+import io.kadai.adapter.systemconnector.camunda.api.impl.Camunda8TaskClaimer;
+import io.kadai.adapter.systemconnector.camunda.api.impl.Camunda8TaskCompleter;
 import io.kadai.adapter.systemconnector.camunda.config.Camunda8System;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.TestExecutionListener;
 
 public class Camunda8TestSetupListener implements TestExecutionListener {
 
-  private static final String CAMUNDA_8_SELF_MANAGED_DEFAULT_REST_ADDRESS = "http://localhost:8088";
-
   @Override
   public void beforeTestMethod(TestContext testContext) {
     CamundaClient client = testContext.getApplicationContext().getBean(CamundaClient.class);
     Camunda8System camunda8System =
         testContext.getApplicationContext().getBean(Camunda8System.class);
-    AdapterManager adapterManager =
-        testContext.getApplicationContext().getBean(AdapterManager.class);
 
     final String camunda8RestAddress = client.getConfiguration().getRestAddress().toString();
     camunda8System.setRestAddress(camunda8RestAddress);
 
-    // When AdapterManager loads C8 SPI, the rest-address is not yet set for tests because
-    // the CamundaClient is not yet initialized.
-    // Therefore, Camunda itself defaults to CAMUNDA_8_SELF_MANAGED_DEFAULT_REST_ADDRESS.
-    // Hence, why we need to overwrite it here with the actual address (due to dynamic port).
-    final OutboundSystemConnector camunda8OutboundSystemConnector =
-        adapterManager
-            .getOutboundSystemConnectors()
-            .remove(CAMUNDA_8_SELF_MANAGED_DEFAULT_REST_ADDRESS);
+    // Each test-suite gets its own Camunda8-Instance
+    // Hence, why we keep adding these new instances
+    // While this technically leads to "old" OutBoundSystemConnectors it's irrelevant
+    // because we look up by key (system-url) anyway
+    AdapterManager adapterManager =
+        testContext.getApplicationContext().getBean(AdapterManager.class);
+    final Camunda8SystemConnectorImpl camunda8SystemConnectorImpl =
+        new Camunda8SystemConnectorImpl(
+            testContext.getApplicationContext().getBean(Camunda8System.class),
+            testContext.getApplicationContext().getBean(Camunda8TaskClaimer.class),
+            testContext.getApplicationContext().getBean(Camunda8TaskCompleter.class),
+            testContext.getApplicationContext().getBean(Camunda8TaskClaimCanceler.class));
 
-    if (camunda8OutboundSystemConnector != null) {
-      adapterManager
-          .getOutboundSystemConnectors()
-          .put(camunda8RestAddress, camunda8OutboundSystemConnector);
-    }
+    adapterManager
+        .getOutboundSystemConnectors()
+        .put(camunda8RestAddress, camunda8SystemConnectorImpl);
   }
 }
