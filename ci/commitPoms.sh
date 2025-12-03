c083b7ec5aeecf2e8feba9dd1c746dc0fbe74d04
jamesrdi <128586627+jamesrdi@users.noreply.github.com>
2025-12-03 13:08:06 +0100

    Closes #280: Snapshot-Pushback is broken (#302)
    * Closes #280: Snapshot-Pushback is broken
    
    * Use github-actions-bot
    
    * Use variable NEW_VERSION and change PR title body and label
    
    * Added permissions

--- ci/commitPoms.sh
+++ ci/commitPoms.sh
@@ -12,8 +12,7 @@ set -e # fail fast
 #H
 #H Requirements:
 #H   current commit is a HEAD commit
-#H   GH_USERNAME - github username / displayname (for git config)
-#H   GH_EMAIL - github email address (for git config)
+#H   GH_TOKEN - GitHub token for authentication
 #H   GITHUB_REF (format refs/tags/v[0-9]+\.[0-9]+\.[0-9]+)
 # Arguments:
 #   $1: exit code
@@ -38,7 +37,7 @@ function increment_version() {
 
 function main() {
   [[ "$1" == '-h' || "$1" == '--help' ]] && helpAndExit 0
-  [[ -z "$GH_EMAIL" || -z "$GH_USERNAME" ]] && helpAndExit 1
+  [[ -z "$GH_TOKEN" ]] && helpAndExit 1
   if [[ "$GITHUB_REF" =~ ^refs/tags/([0-9]+\.[0-9]+\.[0-9]+)/([0-9]+\.[0-9]+\.[0-9]+)$ ]]; then
     #check if tagged commit is a head commit of any branch
     commit=$(git ls-remote -q -t origin | grep "$GITHUB_REF" | cut -c1-40)
@@ -56,17 +55,29 @@ function main() {
       exit 1
     fi
     set -x
-    git config --global user.email "$GH_EMAIL"
-    git config --global user.name "$GH_USERNAME"
+    git config --global user.email "github-actions[bot]@users.noreply.github.com"
+    git config --global user.name "github-actions[bot]"
 
     #commit all poms
     git checkout "$branch"
+    NEXT1=$(increment_version "${BASH_REMATCH[1]}")
+    NEXT2=$(increment_version "${BASH_REMATCH[2]}")
+    NEW_VERSION="${NEXT1}-SNAPSHOT/${NEXT2}-SNAPSHOT"
+    PR_BRANCH="auto/version-bump-${NEXT1}-$(date +%s)"
+
+    git checkout -b "$PR_BRANCH"
     git add "./*pom.xml"
     for file in "$@"; do
       [[ -n "$file" ]] && git add "$file"
     done
-    git commit -m "Updated poms to version $(increment_version "${BASH_REMATCH[@]:1:1}")-SNAPSHOT/$(increment_version "${BASH_REMATCH[@]:2:2}")-SNAPSHOT"
-    git push
+    git commit -m "Updated poms to version ${NEW_VERSION}"
+    git push origin "$PR_BRANCH"
+
+    gh pr create \
+          --base "$branch" \
+          --title "chore: version bump to ${NEW_VERSION}" \
+          --body "Automated version update after release. All tests have passed in the release workflow." \
+          --label "dependencies"
   else
     echo "Nothing to push - this is not a release!"
   fi
