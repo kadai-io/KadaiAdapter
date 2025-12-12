0c9154b9fc9d55af5d52e099d9867c40aa40e226
James Reynaldi <james.reynaldi@envite.de>
2025-12-12 13:33:39 +0100

    Remove redundant check

--- kadai-adapter-kadai-connector/src/main/java/io/kadai/adapter/kadaiconnector/api/impl/TaskInformationMapper.java
+++ kadai-adapter-kadai-connector/src/main/java/io/kadai/adapter/kadaiconnector/api/impl/TaskInformationMapper.java
@@ -203,22 +203,9 @@ public class TaskInformationMapper {
 
   private void setTimestampsInKadaiTask(TaskImpl kadaiTask, ReferencedTask camundaTask) {
     Instant now = Instant.now();
-    Instant created = convertStringToInstant(camundaTask.getCreated(), now);
-    kadaiTask.setCreated(created);
-
-    String due = camundaTask.getDue();
-    if (due == null || due.isEmpty() || "null".equals(due)) {
-      kadaiTask.setDue(null);
-    } else {
-      kadaiTask.setDue(convertStringToInstant(due, null));
-    }
-
-    String planned = camundaTask.getPlanned();
-    if (planned == null || planned.isEmpty() || "null".equals(planned)) {
-      kadaiTask.setPlanned(null);
-    } else {
-      kadaiTask.setPlanned(convertStringToInstant(planned, null));
-    }
+    kadaiTask.setCreated(convertStringToInstant(camundaTask.getCreated(), now));
+    kadaiTask.setDue(convertStringToInstant(camundaTask.getDue(), null));
+    kadaiTask.setPlanned(convertStringToInstant(camundaTask.getPlanned(), null));
   }
 
   private Instant convertStringToInstant(String strTimestamp, Instant defaultTimestamp) {
