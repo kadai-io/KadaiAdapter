c301a6f7a5b2403bf62a165d9e599ea3fa55aaef
grittaweisheit <44833037+grittaweisheit@users.noreply.github.com>
2026-01-14 16:19:56 +0100

    Closes #356: replace rest template with camunda client
    * task#356: replace RestTemplate with CamundaClient
    
    * task#356: adjust error message for Get Task
    
    * task#356: refine Exceptions and send variables with complete task commands
    
    * remove unused import
    
    * task#356: catch ClientException
    
    * task#356: fix sending variables on complete and matching test case

--- kadai-adapter-camunda8-parent/kadai-adapter-camunda-8-system-connector/src/main/java/io/kadai/adapter/systemconnector/camunda/api/impl/Camunda8UtilRequester.java
+++ kadai-adapter-camunda8-parent/kadai-adapter-camunda-8-system-connector/src/main/java/io/kadai/adapter/systemconnector/camunda/api/impl/Camunda8UtilRequester.java
@@ -1,50 +1,30 @@
 package io.kadai.adapter.systemconnector.camunda.api.impl;
 
+import io.camunda.client.CamundaClient;
+import io.camunda.client.api.command.ClientException;
 import io.kadai.adapter.systemconnector.api.ReferencedTask;
-import io.kadai.adapter.systemconnector.camunda.config.Camunda8System;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
 
 /** Util class for Camunda 8 requests used in multiple components of Camunda8SystemConnectorImpl. */
 public class Camunda8UtilRequester {
 
-  private Camunda8UtilRequester() {}
-
   private static final Logger LOGGER = LoggerFactory.getLogger(Camunda8UtilRequester.class);
 
-  public static boolean isTaskExisting(
-      Camunda8HttpHeaderProvider httpHeaderProvider,
-      RestTemplate restTemplate,
-      Camunda8System camunda8System,
-      String userTaskKey) {
+  private Camunda8UtilRequester() {}
 
-    String requestUrl =
-        camunda8System.getRestAddress()
-            + Camunda8SystemConnectorImpl.URL_GET_CAMUNDA8_USER_TASKS
-            + userTaskKey;
-
-    HttpEntity<Void> requestEntity =
-        new HttpEntity<>(httpHeaderProvider.getHttpHeadersForCamunda8TasklistApi());
-
+  public static boolean isTaskExisting(CamundaClient camundaClient, Long userTaskKey) {
     try {
-      restTemplate.exchange(requestUrl, HttpMethod.GET, requestEntity, String.class);
-    } catch (HttpStatusCodeException ex) {
-      boolean isNotExisting = HttpStatus.NOT_FOUND.equals(ex.getStatusCode());
-      if (isNotExisting) {
-        LOGGER.debug("Camunda 8 Task {} is not existing. Returning silently", userTaskKey);
-      }
-      return isNotExisting;
+      camundaClient.newUserTaskGetRequest(userTaskKey).send().join();
+      return true;
+    } catch (ClientException e) {
+      LOGGER.debug("Camunda 8 Task {} was not found. Returning silently", userTaskKey);
+      return false;
     }
-    return false;
   }
 
-  public static String getUserTaskKeyFromReferencedTask(ReferencedTask task) {
+  public static Long getUserTaskKeyFromReferencedTask(ReferencedTask task) {
     String id = task.getId();
-    return id.substring(id.lastIndexOf('-') + 1);
+    return Long.parseLong(id.substring(id.lastIndexOf('-') + 1));
   }
 }
