8636fd08db6b8ace29789459fedac76f7a10d307
Julian Bruder <julian.bruder@outlook.com>
2026-01-22 10:53:02 +0100

    fix#221: test setting action-flag on outbound completion

--- kadai-adapter-camunda8-parent/kadai-adapter-camunda-8-system-connector/src/test/java/io/kadai/adapter/systemconnector/camunda/api/impl/Camunda8TaskCompleterTest.java
+++ kadai-adapter-camunda8-parent/kadai-adapter-camunda-8-system-connector/src/test/java/io/kadai/adapter/systemconnector/camunda/api/impl/Camunda8TaskCompleterTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.camunda.client.CamundaClient;
 import io.camunda.client.api.response.ProcessInstanceEvent;
+import io.camunda.client.api.worker.JobHandler;
+import io.camunda.client.api.worker.JobWorker;
 import io.camunda.process.test.api.CamundaAssert;
 import io.kadai.adapter.systemconnector.camunda.Camunda8TestUtil;
 import io.kadai.adapter.systemconnector.camunda.KadaiAdapterCamunda8SpringBootTest;
@@ -19,7 +21,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
- * Tests for completing tasks from Kadai to Camunda 8. Tests the synchronisation of status when
+ * Tests for completing tasks from Kadai to Camunda 8. Tests the synchronization of status when
  * tasks get completed in Kadai.
  */
 @KadaiAdapterCamunda8SpringBootTest
@@ -71,6 +73,56 @@ class Camunda8TaskCompleterTest {
 
   @Test
   @WithAccessId(user = "admin")
+  void should_SetCompletedByKadaiAction_When_KadaiTaskIsCompleted() throws Exception {
+    final JobHandler verificationHandler =
+        (jobClient, job) -> {
+          final String action = job.getUserTask().getAction();
+          assertThat(action).isEqualTo("completed-by-kadai");
+          jobClient.newCompleteCommand(job).send().join();
+        };
+
+    try (final JobWorker worker =
+        client
+            .newWorker()
+            .jobType("kadai-receive-task-completed-event")
+            .handler(verificationHandler)
+            .open()) {
+      kadaiAdapterTestUtil.createWorkbasket("GPK_KSC", "DOMAIN_A");
+      kadaiAdapterTestUtil.createClassification("L11010", "DOMAIN_A");
+      client
+          .newDeployResourceCommand()
+          .addResourceFromClasspath("processes/sayHello.bpmn")
+          .send()
+          .join();
+
+      final ProcessInstanceEvent processInstance =
+          client
+              .newCreateInstanceCommand()
+              .bpmnProcessId("Test_Process")
+              .latestVersion()
+              .send()
+              .join();
+      CamundaAssert.assertThat(processInstance).isActive();
+
+      final List<TaskSummary> tasks = kadaiEngine.getTaskService().createTaskQuery().list();
+      final Task kadaiTask = kadaiEngine.getTaskService().getTask(tasks.get(0).getId());
+      kadaiEngine.getTaskService().claim(kadaiTask.getId());
+      kadaiEngine.getTaskService().completeTask(kadaiTask.getId());
+
+      final Task completedKadaiTask = kadaiEngine.getTaskService().getTask(kadaiTask.getId());
+      assertThat(completedKadaiTask.getState()).isEqualTo(TaskState.COMPLETED);
+      String externalId = kadaiTask.getExternalId();
+
+      long camundaTaskKey = ReferencedTaskCreator.extractUserTaskKeyFromTaskId(externalId);
+      camunda8TestUtil.waitUntil(
+          () -> "COMPLETED".equals(camunda8TestUtil.getCamundaTaskStatus(camundaTaskKey)));
+
+      CamundaAssert.assertThat(processInstance).isCompleted();
+    }
+  }
+
+  @Test
+  @WithAccessId(user = "admin")
   void should_CancelCamundaTask_When_KadaiTaskIsCancelled() throws Exception {
     kadaiAdapterTestUtil.createWorkbasket("GPK_KSC", "DOMAIN_A");
     kadaiAdapterTestUtil.createClassification("L11010", "DOMAIN_A");
@@ -106,5 +158,58 @@ class Camunda8TaskCompleterTest {
         () -> "COMPLETED".equals(camunda8TestUtil.getCamundaTaskStatus(camundaTaskKey)));
 
     CamundaAssert.assertThat(processInstance).isCompleted();
+  }
+
+  @Test
+  @WithAccessId(user = "admin")
+  void should_SetCompletedByKadaiAction_When_KadaiTaskIsCancelled() throws Exception {
+    final JobHandler verificationHandler =
+        (jobClient, job) -> {
+          final String action = job.getUserTask().getAction();
+          assertThat(action).isEqualTo("completed-by-kadai");
+          jobClient.newCompleteCommand(job).send().join();
+        };
+
+    try (final JobWorker worker =
+        client
+            .newWorker()
+            .jobType("kadai-receive-task-cancelled-event")
+            .handler(verificationHandler)
+            .open()) {
+      kadaiAdapterTestUtil.createWorkbasket("GPK_KSC", "DOMAIN_A");
+      kadaiAdapterTestUtil.createClassification("L11010", "DOMAIN_A");
+      client
+          .newDeployResourceCommand()
+          .addResourceFromClasspath("processes/sayHello.bpmn")
+          .send()
+          .join();
+
+      final ProcessInstanceEvent processInstance =
+          client
+              .newCreateInstanceCommand()
+              .bpmnProcessId("Test_Process")
+              .latestVersion()
+              .send()
+              .join();
+
+      CamundaAssert.assertThat(processInstance).isActive();
+
+      final List<TaskSummary> tasks = kadaiEngine.getTaskService().createTaskQuery().list();
+      assertThat(tasks).hasSize(1);
+
+      final Task kadaiTask = kadaiEngine.getTaskService().getTask(tasks.get(0).getId());
+
+      kadaiEngine.getTaskService().cancelTask(kadaiTask.getId());
+
+      final Task completedKadaiTask = kadaiEngine.getTaskService().getTask(kadaiTask.getId());
+      assertThat(completedKadaiTask.getState()).isEqualTo(TaskState.CANCELLED);
+      String externalId = kadaiTask.getExternalId();
+
+      long camundaTaskKey = ReferencedTaskCreator.extractUserTaskKeyFromTaskId(externalId);
+      camunda8TestUtil.waitUntil(
+          () -> "COMPLETED".equals(camunda8TestUtil.getCamundaTaskStatus(camundaTaskKey)));
+
+      CamundaAssert.assertThat(processInstance).isCompleted();
+    }
   }
 }
