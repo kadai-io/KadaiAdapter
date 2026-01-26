315dcd8e9aa07486e47a9718f4c2f5aa75f3a1a2
Julian Bruder <julian.bruder@outlook.com>
2026-01-26 17:36:48 +0100

    fix#376: swap class-bound outbox datasource for lazy static variant

---
+++ kadai-adapter-camunda-outbox-rest/src/main/java/io/kadai/adapter/camunda/outbox/rest/config/OutboxDataSource.java
@@ -0,0 +1,46 @@
+package io.kadai.adapter.camunda.outbox.rest.config;
+
+import io.kadai.adapter.camunda.OutboxRestConfiguration;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class OutboxDataSource {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(OutboxDataSource.class);
+  private static volatile DataSource dataSource;
+
+  public static DataSource get() {
+    DataSource local = dataSource;
+    if (local == null) {
+      synchronized (OutboxDataSource.class) {
+        local = dataSource;
+        if (local == null) {
+          dataSource = local = getDataSourceFromPropertiesFile();
+        }
+      }
+    }
+    return local;
+  }
+
+  private static DataSource getDataSourceFromPropertiesFile() {
+    String jndiUrl = OutboxRestConfiguration.getOutboxDatasourceJndi();
+    if (jndiUrl != null) {
+      try {
+        return (DataSource) new InitialContext().lookup(jndiUrl);
+      } catch (NamingException e) {
+        LOGGER.error("Failed creating initial context. Rethrowing as RuntimeException.", e);
+        throw new RuntimeException(e);
+      }
+    } else {
+      return new PooledDataSource(
+          OutboxRestConfiguration.getOutboxDatasourceDriver(),
+          OutboxRestConfiguration.getOutboxDatasourceUrl(),
+          OutboxRestConfiguration.getOutboxDatasourceUsername(),
+          OutboxRestConfiguration.getOutboxDatasourcePassword());
+    }
+  }
+}
