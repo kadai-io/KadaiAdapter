8548fe754f6a21c16d37850a454c5cd4af4af7c8
Julian Bruder <94740838+bruderj15@users.noreply.github.com>
2026-03-27 13:57:55 +0100

    Closes #403 - Remove C7 Outbox JNDI-Support (#428)
    * task#403: remove jndi
    
    * task#403: remove unused logger

--- kadai-adapter-camunda7-parent/kadai-adapter-camunda-outbox-rest/src/main/java/io/kadai/adapter/camunda/outbox/rest/config/OutboxDataSource.java
+++ kadai-adapter-camunda7-parent/kadai-adapter-camunda-outbox-rest/src/main/java/io/kadai/adapter/camunda/outbox/rest/config/OutboxDataSource.java
@@ -1,16 +1,11 @@
 package io.kadai.adapter.camunda.outbox.rest.config;
 
 import io.kadai.adapter.camunda.OutboxRestConfiguration;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 import org.apache.ibatis.datasource.pooled.PooledDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public final class OutboxDataSource {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(OutboxDataSource.class);
   private static volatile DataSource dataSource;
 
   private OutboxDataSource() {}
@@ -29,20 +24,10 @@ public final class OutboxDataSource {
   }
 
   private static DataSource getDataSourceFromPropertiesFile() {
-    String jndiUrl = OutboxRestConfiguration.getOutboxDatasourceJndi();
-    if (jndiUrl != null) {
-      try {
-        return (DataSource) new InitialContext().lookup(jndiUrl);
-      } catch (NamingException e) {
-        LOGGER.error("Failed creating initial context. Rethrowing as RuntimeException.", e);
-        throw new RuntimeException(e);
-      }
-    } else {
-      return new PooledDataSource(
-          OutboxRestConfiguration.getOutboxDatasourceDriver(),
-          OutboxRestConfiguration.getOutboxDatasourceUrl(),
-          OutboxRestConfiguration.getOutboxDatasourceUsername(),
-          OutboxRestConfiguration.getOutboxDatasourcePassword());
-    }
+    return new PooledDataSource(
+        OutboxRestConfiguration.getOutboxDatasourceDriver(),
+        OutboxRestConfiguration.getOutboxDatasourceUrl(),
+        OutboxRestConfiguration.getOutboxDatasourceUsername(),
+        OutboxRestConfiguration.getOutboxDatasourcePassword());
   }
 }
