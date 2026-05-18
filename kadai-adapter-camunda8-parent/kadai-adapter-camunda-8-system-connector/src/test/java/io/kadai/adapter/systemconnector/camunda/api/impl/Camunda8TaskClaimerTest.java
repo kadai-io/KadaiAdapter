dc07313b29a8899edc94b6a88afcd857a842ca9b
Julian Bruder <julian.bruder@outlook.com>
2026-05-18 10:09:40 +0200

    task#311: remove giga laky & redundant test

--- kadai-adapter-camunda8-parent/kadai-adapter-camunda-8-system-connector/src/test/java/io/kadai/adapter/systemconnector/camunda/api/impl/Camunda8TaskClaimerTest.java
+++ kadai-adapter-camunda8-parent/kadai-adapter-camunda-8-system-connector/src/test/java/io/kadai/adapter/systemconnector/camunda/api/impl/Camunda8TaskClaimerTest.java
@@ -273,65 +273,6 @@ class Camunda8TaskClaimerTest {
 
     @Test
     @WithAccessId(user = "admin")
-    void should_ClaimAlreadyClaimedCamundaTask_When_ClaimKadaiTask() throws Exception {
-      kadaiAdapterTestUtil.createWorkbasket("GPK_KSC", "DOMAIN_A");
-      kadaiAdapterTestUtil.createClassification("L11010", "DOMAIN_A");
-
-      // create new Tenant and add user to it (user needs access to all tenants)
-      final String tenant1 = "tenant1";
-      final String allTenantsUser = "demo";
-      client.newCreateTenantCommand().tenantId(tenant1).name(tenant1).send().join();
-      client
-          .newAssignUserToTenantCommand()
-          .username(allTenantsUser)
-          .tenantId(tenant1)
-          .send()
-          .join();
-
-      client
-          .newDeployResourceCommand()
-          .addResourceFromClasspath("processes/sayHello.bpmn")
-          .tenantId(tenant1)
-          .send()
-          .join();
-
-      final ProcessInstanceEvent processInstance =
-          client
-              .newCreateInstanceCommand()
-              .bpmnProcessId("Test_Process")
-              .latestVersion()
-              .tenantId(tenant1)
-              .send()
-              .join();
-
-      CamundaAssert.assertThat(processInstance).isActive();
-
-      final List<TaskSummary> tasks = kadaiEngine.getTaskService().createTaskQuery().list();
-      assertThat(tasks).hasSize(1);
-
-      final Task kadaiTask = kadaiEngine.getTaskService().getTask(tasks.get(0).getId());
-      String externalId = kadaiTask.getExternalId();
-      long camundaTaskKey = ReferencedTaskCreator.extractUserTaskKeyFromTaskId(externalId);
-
-      camunda8TestUtil.assignCamundaTask(camundaTaskKey, "user-1-1");
-      camunda8TestUtil.waitUntil(
-          () -> "user-1-1".equals(camunda8TestUtil.getCamundaTaskAssignee(camundaTaskKey)));
-
-      assertThat(kadaiTask.getState()).isEqualTo(io.kadai.task.api.TaskState.READY);
-      kadaiEngine.getTaskService().claim(kadaiTask.getId());
-
-      final Task claimedKadaiTask = kadaiEngine.getTaskService().getTask(kadaiTask.getId());
-      assertThat(claimedKadaiTask.getState()).isEqualTo(io.kadai.task.api.TaskState.CLAIMED);
-      assertThat(claimedKadaiTask.getOwner()).isEqualTo("admin");
-
-      camunda8TestUtil.waitUntil(
-          () -> "admin".equals(camunda8TestUtil.getCamundaTaskAssignee(camundaTaskKey)));
-
-      CamundaAssert.assertThat(processInstance).isActive();
-    }
-
-    @Test
-    @WithAccessId(user = "admin")
     void should_CancelClaimCamundaTask_When_KadaiTaskIsCancelClaimed() throws Exception {
       kadaiAdapterTestUtil.createWorkbasket("GPK_KSC", "DOMAIN_A");
       kadaiAdapterTestUtil.createClassification("L11010", "DOMAIN_A");
