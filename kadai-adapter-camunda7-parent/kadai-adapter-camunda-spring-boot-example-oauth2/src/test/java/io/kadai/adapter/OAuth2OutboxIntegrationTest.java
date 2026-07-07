b6f24a3ee5a51d605d9189299880320197a602a1
Julian Bruder <julian.bruder@outlook.com>
2026-07-07 12:54:47 +0200

    fix test docs

--- kadai-adapter-camunda7-parent/kadai-adapter-camunda-spring-boot-example-oauth2/src/test/java/io/kadai/adapter/OAuth2OutboxIntegrationTest.java
+++ kadai-adapter-camunda7-parent/kadai-adapter-camunda-spring-boot-example-oauth2/src/test/java/io/kadai/adapter/OAuth2OutboxIntegrationTest.java
@@ -43,6 +43,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
